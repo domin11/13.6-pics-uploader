3936c5a5416a3de991ec0efcf45b935e589ff661
Dominik Soboń <dominik.sobon@gmail.com>
2018-09-03 23:46:28 +0200

    Add start.html

--- modules/handlers.js
+++ modules/handlers.js
@@ -1,7 +1,12 @@
-exports.upload = function(request, response) {
-    console.log("Rozpoczynam obsługę żądania upload.");
-    response.write("Rozpoczynam upload!");
-    response.end();
+var fs = require('fs');
+
+exports.welcome = function(request, response) {
+    console.log("Rozpoczynam obsługę żądania welcome.");
+    fs.readFile('templates/start.html', function(err, html) {
+        response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+        response.write(html);
+        response.end();
+    });
 }
 
 exports.welcome = function(request, response) {
