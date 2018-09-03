5bde1c345414d3a18da6a7d6cc98e246f7080a00
Dominik Soboń <dominik.sobon@gmail.com>
2018-09-04 00:47:50 +0200

    Modify js files

--- modules/server.js
+++ modules/server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var colors = require('colors');
 
-var handlers = require('./handlers'); // nasz moduł
+var handlers = require('./handlers');
 
 function start() {
   function onRequest(request, response) {
@@ -10,14 +10,17 @@ function start() {
 
     response.writeHead(200, {"Content-Type": "text/plain; charset=utf-8"});
 
-    switch (request.url) { // switch rozróżniający zapytania
-        case '/':
-        case '/start':
-            handlers.welcome(request, response);
-            break;
-        case '/upload':
-            handlers.upload(request, response);
-            break;
+switch (request.url) {
+	case '/':
+	case '/start':
+		handlers.welcome(request, response);
+		break;
+	case '/upload':
+		handlers.upload(request, response);
+		break;
+	case '/show':
+		handlers.show(request, response);
+    break;
         default:
             handlers.error(request, response);
     }
