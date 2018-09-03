37b5bcba4109716f794036c0512ecc86df2c8030
Dominik Soboń <dominik.sobon@gmail.com>
2018-09-03 23:50:06 +0200

    Modify handlers.js

--- modules/handlers.js
+++ modules/handlers.js
@@ -1,4 +1,17 @@
 var fs = require('fs');
+var formidable = require('formidable');
+
+exports.upload = function(request, response) {
+    console.log("Rozpoczynam obsługę żądania upload.");
+    var form = new formidable.IncomingForm();
+    form.parse(request, function(error, fields, files) {
+        fs.renameSync(files.upload.path, "test.png");
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write("received image:<br/>");
+        response.write("<img src='/show' />");
+        response.end();
+    });
+}
 
 exports.welcome = function(request, response) {
     console.log("Rozpoczynam obsługę żądania welcome.");
@@ -7,12 +20,6 @@ exports.welcome = function(request, response) {
         response.write(html);
         response.end();
     });
-}
-
-exports.welcome = function(request, response) {
-    console.log("Rozpoczynam obsługę żądania welcome.");
-    response.write("Witaj na stronie startowej!");
-    response.end();
 }
 
 exports.error = function(request, response) {
